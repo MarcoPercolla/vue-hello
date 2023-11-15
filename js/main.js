37fb71a8aa10168b9fa42b2a30811776d9c6e043
ReDMallard <marco@titori.it>
2023-11-15 23:35:16 +0100

    esercizio completato

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("ok");
+const { createApp } = Vue ;
+
+createApp({
+    data () {
+        return {
+            message: `Missione compiuta`,
+            immagine: `https://t3.ftcdn.net/jpg/01/08/56/20/360_F_108562024_LoOUmSD9RmW7oXpVIhWvEQvJuCbetU15.jpg`,
+
+        }
+
+    }
+}).mount(`#app`)
